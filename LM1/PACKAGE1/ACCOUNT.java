3ad05c0f8bf09184a8229256f3fdc206a848be73
Ara Gonzaga <aramazing318@gmail.com>
2023-03-13 01:19:25 +0800

    Create ACCOUNT.java

---
+++ LM1/PACKAGE1/ACCOUNT.java
@@ -0,0 +1,10 @@
+package register;
+
+public class Account {
+	String account_username;
+	String account_password;
+	Account(String username, String password){
+		account_username = username;
+		account_password = password;
+	}
+}
