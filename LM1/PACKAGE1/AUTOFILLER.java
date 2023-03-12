5e188081124ea4510c082ad9225e2a35786da803
Ara Gonzaga <aramazing318@gmail.com>
2023-03-13 01:18:49 +0800

    Create AUTOFILLER.java

---
+++ LM1/PACKAGE1/AUTOFILLER.java
@@ -0,0 +1,13 @@
+package register;
+
+import java.awt.Component;
+
+public class Autofiller {
+	// OBJECTS THAT SUPPORT THE ARRAYLIST
+	String username;
+	String password;
+		Autofiller(String usr, String pass){
+			username = usr;
+			password = pass;
+		}
+}
